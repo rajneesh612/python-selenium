1b50702e0e326de8017d365457560f024a4db881
Rajneesh Sharma <74241338+rajneesh612@users.noreply.github.com>
2023-03-21 18:47:27 +0530

    Add files via upload
    how to scroll using java script executer

--- JavaScript.py
+++ JavaScript.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 
@@ -25,14 +27,31 @@ elif(browser_name == "edge"):
 browser.get('https://www.amazon.in/')
 
 
-browser.find_element(By.XPATH,"//i[@class='hm-icon nav-sprite']").click()
+# browser.find_element(By.XPATH,"//i[@class='hm-icon nav-sprite']").click()
+#
+# best_sellers = browser.find_element(By.LINK_TEXT,'Best Sellers')
+# browser.execute_script("arguments[0].click();",best_sellers)
+# title= browser.execute_script('return document.title;')
+# print(title)
+# browser.execute_script("alert('hello');")
 
+#inner_text= browser.execute_script("return document.documentElement.innerText;")
+print(browser.execute_script('return document.documentElement.innerText'))
+#print(inner_text)
+browser.find_element(By.XPATH,"//i[@class='hm-icon nav-sprite']").click()
 best_sellers = browser.find_element(By.LINK_TEXT,'Best Sellers')
 browser.execute_script("arguments[0].click();",best_sellers)
 title= browser.execute_script('return document.title;')
-print(title)
-browser.execute_script("alert('hello');")
-
-inner_text= browser.execute_script('return document.documentElement.innerText')
-print(inner_text)
+# print(title)
+#browser.execute_script("alert('hello');")
+#browser.execute_script('window.scrollTo(0, document.body.scrollHeight);')
 time.sleep(1)
+
+#element= WebDriverWait(browser,10).\
+#    until(EC.presence_of_element_located(By.XPATH,"//h2[normalize-space()='Bestsellers in Office Products']"))
+
+element= browser.find_element(By.XPATH,"//h2[normalize-space()='Bestsellers in Office Products']")
+
+browser.execute_script("arguments[0].scrollIntoView(true);", element)
+
+time.sleep(5)
